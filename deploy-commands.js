b29849caa2201a4fdff71014873360e5d015a75c
pp7170934-ai <pp7170934@gmail.com>
2026-05-02 18:38:17 +0200

    add deploy-commands.js

---
+++ deploy-commands.js
@@ -0,0 +1,28 @@
+const { REST, Routes } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+require('dotenv').config();
+
+const commands = [];
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(f => f.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const command = require(path.join(commandsPath, file));
+  commands.push(command.data.toJSON());
+}
+
+const rest = new REST().setToken(process.env.DISCORD_TOKEN);
+
+(async () => {
+  try {
+    console.log(`Registering ${commands.length} slash commands...`);
+    await rest.put(
+      Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+      { body: commands }
+    );
+    console.log('Slash commands registered successfully.');
+  } catch (err) {
+    console.error(err);
+  }
+})();
